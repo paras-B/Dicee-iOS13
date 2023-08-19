119a678448cc9875e80f835e62b3b9bac4b6f9b5
paras-B <parasbhatia999@gmail.com>
2023-08-19 00:07:55 -0400

    VariableConnection

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    
+    @IBOutlet weak var diceImageViewRight: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+    }
+    
 }
 
